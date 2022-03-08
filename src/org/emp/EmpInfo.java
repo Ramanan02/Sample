01369d2dd1262dfde952acddf96ece92b0e98f52
Ramanan02 <ramananofficialmail@gmail.com>
2022-03-08 13:35:54 +0530

    Hi

---
+++ src/org/emp/EmpInfo.java
@@ -0,0 +1,30 @@
+package org.emp;
+
+public class EmpInfo {
+	
+public static void main(String[] args) {
+	
+int empid = 12345;
+System.out.println(" EmpId is "+ empid);
+
+String empname = "YYY" ;
+System.out.println(" EmpName is "+ empname);
+
+String empemail	= "YYY@gmail.com";
+System.out.println(" EmpEmail is "+ empemail);
+	
+long empphoneno = 9876543210l ;
+System.out.println(" EmpPhoneno is "+ empphoneno);
+
+float empsalary = 50/3f ;
+System.out.println(" EmpSalary is "+ empsalary);
+
+String empgender = "Male" ;
+System.out.println(" EmpGender is "+ empgender);
+
+String empcity = " Chennai " ;
+System.out.println(" EmpCity is "+ empcity);
+
+}	
+	
+}
